d6c6d35c02fcbf4c0efad6e3c399398238e1cd0e
Yamansaini0405 <sainiyaman14@gmail.com>
2025-08-19 23:11:49 +0530

    initial commit

---
+++ sms/sms/src/main/java/com/yaman/sms/SmsPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yaman.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ sms/sms/src/test/java/com/yaman/sms/SmsPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yaman.sms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
